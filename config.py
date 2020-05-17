d989a91ebd2bff6e59703e28262cc856fa044d04
Sagging Rufus <Justinmurphy92@gmail.com>
2020-05-17 12:52:44 -0300

    test config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     DB_USER = 'username'
     DB_PASS = 'password'
 
-    DISCORD_TOKEN = 'discord_bot_token_here'
+    DISCORD_TOKEN = 'NzExNTk0MDM0NzgyNTM1NzMw.XsFSSQ.lo8UIBbmITTza1QNO8ZOV7WsfSk'
 
     SRL_NICK = 'SahasrahTest'
     SRL_PASSWORD = 'password'
